d4751539cea5348732fc7ac5068a84f9f90d3ebd
weirenye <57995805+weirenye@users.noreply.github.com>
2020-02-20 18:16:46 +0800

    Updated crawler.java only
    updated to remove bug that causes out of index error

--- reddit/src/test/java/reddit/Crawler.java
+++ reddit/src/test/java/reddit/Crawler.java
@@ -9,6 +9,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import net.dean.jraw.RedditClient;
 import net.dean.jraw.oauth.Credentials;
@@ -25,48 +26,61 @@ import net.dean.jraw.models.TimePeriod;
 public class Crawler {
 
 	public static void main(String[] args) throws IOException {
-
-		FileWriter csvWriter = new FileWriter("new.csv");
-		csvWriter.append("Upvotes");
-		csvWriter.append(",");
-		csvWriter.append("Date");
-		csvWriter.append(",");
-		csvWriter.append("Text");
-		csvWriter.append("\n");
+		//creating reddit.csv file
+		FileWriter csvWriter = new FileWriter("reddit.csv");
+		//adding headers to files
+		csvWriter.append("NoOfComments,URL,Date,Upvotes,Title,Text\n");
 		
-	
+		//used to log into reddit
 		UserAgent userAgent = new UserAgent("bot", "com.example.usefulbot", "v0.1", "mattbdean");
-		Credentials credentials = Credentials.script("tempcrawler", "temptemp",
-			    "LIwSbI8F6-1H5A", "dqglRyYhMd4S-bZA4mAnyQfl6_k");
+		Credentials credentials = Credentials.script("tempcrawler", "temptemp", "LIwSbI8F6-1H5A", "dqglRyYhMd4S-bZA4mAnyQfl6_k");
 		NetworkAdapter adapter = new OkHttpNetworkAdapter(userAgent);
-
+		
+		//temporary search
+		String tempSearchQuery;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Search Query: ");
+		tempSearchQuery = sc.nextLine();
+		sc.close();
+		System.out.println("Search Query: " + tempSearchQuery);
+		
+		
 		// Authenticate and get a RedditClient instance
 		RedditClient reddit = OAuthHelper.automatic(adapter, credentials);
-		//Subreddit sr = reddit.subreddit("RocketLeague").about();
-		List<SubredditSearchResult> mylists = reddit.searchSubredditsByName("Climate change").subList(0, 4);
-		for (SubredditSearchResult s : mylists) {
-			//Subreddit sr = reddit.subreddit(s.getName()).about();
-			System.out.println(s.getName());
-
-			DefaultPaginator<Submission> paginator = reddit.subreddit(s.getName()).posts().sorting(SubredditSort.TOP).limit(3).build();
 		
-			Listing<Submission> posts;
-			while((posts = paginator.next()) != null) {
-				for(Submission sub: posts) {
-					System.out.println(sub.getCreated());
-					System.out.println(sub.getScore());
-					System.out.println(sub.getSelfText());
-					String mys = sub.getCreated() + "," + sub.getScore() + "," + sub.getSelfText(); 
-					csvWriter.append(mys);
-				    csvWriter.append("\n");
-				}
-			}
+		//search for tempSearchQuery input from above in Subreddit and limit to 10 Subreddits
+		
+		int lastIndx = reddit.searchSubredditsByName(tempSearchQuery).size();
+		int currIndx = 10;
+		if (lastIndx < 10 )
+		{
+			currIndx = lastIndx;
+		}
+		
+		List<SubredditSearchResult> mylists = reddit.searchSubredditsByName(tempSearchQuery).subList(0, currIndx);
+		
+		
+		
+		for (SubredditSearchResult s : mylists) {
+			//limit it to top 3 search results for each subreddit
+			DefaultPaginator<Submission> paginator = reddit.subreddit(s.getName()).posts().sorting(SubredditSort.TOP).limit(3).build();
 			
-		    
+			//writing into csv file
+			Listing<Submission> posts;
+				for(Submission sub: posts = paginator.next()) {
+					String TextRemoveChar, TitleRemoveChar;
+					TextRemoveChar = sub.getSelfText();
+					TitleRemoveChar = sub.getTitle();
+					TextRemoveChar = TextRemoveChar.replace("\n", "").replace(",", "_");
+					TitleRemoveChar = TitleRemoveChar.replace("\n", "").replace(",", "_");
+					String mys = sub.getCommentCount() + "," + "https://www.reddit.com" + sub.getPermalink() + "," + sub.getCreated() + "," + sub.getScore() + "," + TitleRemoveChar + "," + TextRemoveChar; 
+					csvWriter.append(mys);
+					csvWriter.append("\n");
+				}
 		}
 		csvWriter.flush();
 		csvWriter.close();
-
+		System.out.println("CSV File closed");
 	}
 
 }
