1d81ba8d469768ad0f061f7c80d0820054724bc6
seymouslee <55991638+seymouslee@users.noreply.github.com>
2020-02-21 23:47:47 +0800

    updated csv formatting
    for CNA

--- CNA/src/cnaScraper.java
+++ CNA/src/cnaScraper.java
@@ -3,14 +3,20 @@ import java.util.Date;
 import java.text.SimpleDateFormat;  
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-public class cnaScraper {
+import com.opencsv.CSVWriter;
+import com.opencsv.exceptions.CsvDataTypeMismatchException;
+import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
+
+public class cnaScraper{
 	public static Document document;
 	
 	public static void main(String args[]){
@@ -43,35 +49,39 @@ public class cnaScraper {
 		try {
 			
 			document = Jsoup.connect(searchlink).get();
+			//for when there is no search results for the term:
 			er = document.getElementsByClass("result-section__error-headline");
 			if (!er.isEmpty()) {
 				print("No search results for "+filename);
 				return;
 			}
 			
-			FileOutputStream fout = new FileOutputStream("CNA_Articles_on_"+filename+".csv");
-			PrintStream csv =new PrintStream(fout);
-			
-			//delimiters set to "=";
-			csv.println("Title=Date=Source=Link=Article");
+			//setup for writing data ito CSV file
+			FileOutputStream fos = new FileOutputStream("CNA_Articles_on_"+filename+".csv");
+			OutputStreamWriter osw = new OutputStreamWriter(fos, StandardCharsets.UTF_8);
+            CSVWriter writer = new CSVWriter(osw);
+            String[] heading = {"Title", "Date", "Source", "Link", "Article"};
+            writer.writeNext(heading);
 
+            //will loop according to how many pages of results are there
 			int totalpages = Integer.parseInt(document.getElementsByClass("pagination__index-value").get(1).text()); 
 			
 			for(int i = 0; i<totalpages; i++ ) {
-				// get links from the results page
-				// start of the new current results page
+				// get links from the current results page
 				Elements results = document.getElementsByClass("teaser__heading").not("teaser__category-container");
 				links = results.select("a[href]");
 				print("Page: "+ i);
 				int numResults = Integer.parseInt(document.getElementsByClass("result-section__index-value").get(0).text());
 				int counter = 0;
+				// for every link in the article, get article details and insert into CSV
 				for (Element link : links) {
 					
 					String linkHref = link.attr("href");
 					String linkText = link.text();
 					linkHref = "https://www.channelnewsasia.com"+linkHref;
 					if(!getDate(linkHref).isEmpty()){
-						csv.println(linkText +"="+getDate(linkHref)+"="+getSource(linkHref)+"="+ linkHref+"=\""+ getArticle(linkHref)+ "\"");
+						String[] line = new String[] {linkText, getDate(linkHref), getSource(linkHref), linkHref, getArticle(linkHref)};
+						writer.writeNext(line);
 						print(linkHref);
 						print(linkText);
 					}
@@ -80,7 +90,6 @@ public class cnaScraper {
 				}
 				
 				//select the link that goes to the next result page
-				//nextPage = document.getElementsByClass("pagination__link is-next").not("section__cta");
 				nextPage = document.getElementsByClass("pagination__link is-next").first();
 				nextLink = "https://www.channelnewsasia.com"+nextPage.attr("href");
 				document = Jsoup.connect(nextLink).get();
